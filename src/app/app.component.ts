e4c65690668b9330559c193b8b15b7600f1746fd
Yannick Coolen <yannick-coolen-1991@hotmail.nl>
2022-01-20 15:03:41 +0100

    committed on 20-01-2022, 15:04

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,21 +8,45 @@ import { Component, Input } from '@angular/core';
 export class AppComponent {
   newMemberName: string = '';
   members: string[] = [];
-  errorMessage: string = ''
+  errorMessage: string = '';
+  numbersOfTeams: number | string = '';
+  teams: string[][] = []
 
   onInput(member: string): void {
     this.newMemberName = member;
+  }
+
+  onTeamSizeInput(value: string): void {
+    this.numbersOfTeams = +value
   }
 
   addMember(): void {
     if (!this.newMemberName) {
       this.errorMessage = "Name field can't be empty."
       return;
-    } 
+    }
 
     this.members.push(this.newMemberName);
     this.newMemberName = '';
     this.errorMessage = '';
   }
 
+  generateTeams(): void {
+    const allMembers = [...this.members];
+
+    if (!this.numbersOfTeams || this.numbersOfTeams <= 0) {
+      return
+    }
+    for (let i = 0; i < this.numbersOfTeams; i++) {
+      const randomIndex = Math.floor(Math.random() * allMembers.length);
+      const member = allMembers.splice(randomIndex, 1)[0];
+
+      if (this.teams[i]) {
+        this.teams[i].push(member);
+      } else {
+        this.teams[i] = [member];
+      }
+      console.log(this.teams)
+    }
+  }
 }
